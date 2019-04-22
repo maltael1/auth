544353ca804dd7003cf226c4993c22ef471a124d
Dmitriy <d.vishnevsky@fast-report.com>
2019-04-22 21:35:46 +0300

    fix user login presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
     validates :password, length: { in: 6..20 }
-    validates :login, uniqueness: true
+    validates :login, uniqueness: true, presence: true
 end
