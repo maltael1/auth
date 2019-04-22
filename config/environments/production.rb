8f2ac6e641ad1d0a7c33a987cd4edc5b84dd8660
Ubuntu <dmitriy@azure.navj32kpeu2u3p00rbpr1q5mkc.ax.internal.cloudapp.net>
2019-04-22 17:50:55 +0000

    Commit from production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,14 @@
 Rails.application.configure do
   config.cache_classes = true
-
+  config.secret_key_base = 'YourSecretKeyHereomgitskey'
   config.eager_load = true
-
   config.discovery_host = 'http://registrator.ya-o.ru/api/v0/register_service' # Хост сервис провайдера
   config.service_token = '6baa78dbd73ae22e88e084967df2fc30' # Токен сервиса аутентификации
 
   config.host = 'http://auth.ya-o.ru'
   config.gate_host = 'http://auth.ya-o.ru/gate'
 
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -24,7 +23,7 @@ Rails.application.configure do
 
   config.i18n.fallbacks = true
 
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :log
 
   config.log_formatter = ::Logger::Formatter.new
 
@@ -36,4 +35,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  RegisterServiceJob.set(wait: 3.seconds).perform_later
 end
